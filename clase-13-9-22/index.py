5ccb0f499fe0442814beb2e4215cc29370a23dd2
enzo agu <enzo.gustavo.agu@gmail.com>
2022-09-22 19:05:39 -0300

    commit-Class

---
+++ clase-13-9-22/index.py
@@ -0,0 +1,20 @@
+from ast import Pass
+
+
+print('algo')
+
+def algo():
+    print('funcion')
+    
+algo()
+
+class Perro:
+    def __init__(self,nombre,raza):
+        self.nombre=nombre
+        self.raza=raza
+        
+        
+        
+perro1=Perro('Laika','ninguna')
+print(perro1  ) 
+    
